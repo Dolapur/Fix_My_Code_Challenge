d7635ffc2a3dd999dad714caffdc32956acb61f9
dolapo adegoju <tichiegoju@gmail.com>
2023-04-17 18:59:27 -0700

    fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-class square():
-    def __init__(self, *args, **kwargs):
+class Square:
+    def __init__(self, *args, **kwargs): 
         self.width = 0
-        self.height = 0    
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -19,7 +19,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
